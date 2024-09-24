d63edaff288df2198ef52a91cd504b69405d7043
Robin-Majumdar <robinmajumdarofficial@gmail.com>
2024-09-24 16:51:32 +0600

    Added a common java function with add some donation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
-document.getElementById("blog-button")
-.addEventListener("click", function () {
-    window.open('important-faq.html', '_blank');
-});
-
-document.getElementById("home-button")
-    .addEventListener("click", function () {
-        window.location.href = ('index.html');
+document.getElementById('blog-button')
+    .addEventListener('click', function () {
+        window.open('important-faq.html', '_blank');
     });
+
+// document.getElementById('home-button')
+//     .addEventListener('click', function () {
+//         window.location.href = ('index.html');
+//     });
+
+document.getElementById('btn-donate-now')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const donationAddMoney = getInputFieldValueById('donation-add-money');
+        const presentBalance = getTextFieldValueById('donation-visible-money');
+
+        if(isNaN(donationAddMoney) || donationAddMoney <=0) {
+            alert("Please enter a valid number!")
+        }
+        else {
+            const newBalance = presentBalance + donationAddMoney;
+            document.getElementById('donation-visible-money').innerText = newBalance;
+            document.getElementById('donation-visible-money').innerText = `${newBalance} BDT`;
+            document.getElementById('donation-add-money').value = '';
+
+        }
+
+
+    });
+
+
+
+
+
+
+
+
+
+
